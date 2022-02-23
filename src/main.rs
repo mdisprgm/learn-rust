67d7c4fd6433684376a22466ea529ec62d953b70
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-02-23 15:50:19 +0900

    cli

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,8 @@
 // mod functions;
 // mod pointer_ref;
 // mod structs;
-mod enums;
+// mod enums;
+mod cli;
 
 fn main() {
     // print::run();
@@ -33,5 +34,6 @@ fn main() {
     // functions::run();
     // pointer_ref::run();
     // structs::run();
-    enums::run();
+    // enums::run();
+    cli::run();
 }
