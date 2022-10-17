c5d328736f6d0c378107547349b84810fc892478
mdisprgm <49188795+mdisprgm@users.noreply.github.com>
2022-10-17 16:22:55 +0900

    remove spell warn

--- src/loops.rs
+++ src/loops.rs
@@ -2,7 +2,7 @@
 pub fn run() {
     let mut count = 0;
 
-    // Inifnite Loop
+    // Infinite Loop
     loop {
         count += 1;
         println!("Number: {}", count);
@@ -15,7 +15,7 @@ pub fn run() {
     // While Loop (FizzBuzz)
     /* while count <= 100 {
         if count % 15 == 0 {
-            println!("fizzbuzz");
+            println!("fizz buzz");
         } else if count % 3 == 0 {
             println!("fizz");
         } else if count % 5 == 0 {
@@ -31,7 +31,7 @@ pub fn run() {
     // For Range
     for x in 0..100 {
         if x % 15 == 0 {
-            println!("fizzbuzz");
+            println!("fizz buzz");
         } else if x % 3 == 0 {
             println!("fizz");
         } else if x % 5 == 0 {
